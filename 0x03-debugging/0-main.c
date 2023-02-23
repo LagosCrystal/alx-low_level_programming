698b4e090513ff2ed0c595cfc2316ef744199abb
German <tobrisegerman@gmail.com>
2023-02-23 13:14:31 -0800

    Debugging 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Test function for positive or negative
- * Return 0
+ * Return: Always 0 (success)
  */
 
 int main(void)
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
-}	
+}
